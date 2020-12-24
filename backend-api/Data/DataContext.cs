cb296f466fd5dc5500918cfbe8d596d099c6b76b
Mohit Singh <mohitsammal1997@gmail.com>
2020-12-24 16:20:14 +0530

    Initial commit

---
+++ backend-api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using backend_api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ backend-api/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace backend_api.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
